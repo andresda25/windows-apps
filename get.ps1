acd89576a03fc36e8a8c08a0271b16b0e1497af7
hndres <andresda1020@gmail.com>
2022-11-26 03:02:00 +0000

    seperated apps.ps1 into two parts

---
+++ get.ps1
@@ -0,0 +1,16 @@
+# Allow powershell scripts from anywhere
+Set-ExecutionPolicy unrestricted -Force
+
+# Chocolatey install
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Winget install
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Script -Name winget-install -Force
+winget-install.ps1
+
+# Get apps.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/andresda25/windows-apps/main/apps.ps1 -Outfile $env:TMP\apps.ps1
+
+# Run apps.ps1 in an elevated shell
+Start-Process PowerShell -Verb runAs -ArgumentList "-file $env:TMP\apps.ps1"
